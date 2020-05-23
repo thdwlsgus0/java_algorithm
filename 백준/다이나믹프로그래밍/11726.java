81ac75f5fb4121465a5426dfab9fa866649114a0
송진현 <thdwlsgus0@naver.com>
2020-05-23 22:01:53 +0900

    다이나믹동적프로그래밍 2*n 타일링

---
+++ 백준/다이나믹프로그래밍/11726.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class 11726 {
+	public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int dp [] = new int[n+10];
+       dp[0]=0;
+       dp[1]= 1;
+       dp[2]= 2;
+       for(int i=3; i<=n; i++) {
+    	   dp[i] = dp[i-1]+dp[i-2];
+    	   dp[i] %= 10007;
+       }
+       System.out.println(dp[n]);
+	}
+}
